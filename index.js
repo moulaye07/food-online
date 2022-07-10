033a277f850e0ccaafaa315bff2eaa39d0ff31b5
moulaye07 <haidaramoulayetouhami@gmail.com>
2022-07-10 20:57:17 +0100

    connexion database et configuration de base

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './config/.env' })
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+mongoose.connect(process.env.MONGO_DB_URL, (err) => {
+    if (!err) console.log("connected");
+    else console.log("error");
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
