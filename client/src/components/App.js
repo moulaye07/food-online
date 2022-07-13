71877fbd41b3b31ecdbfc8fc1698c567e496e081
moulaye07 <haidaramoulayetouhami@gmail.com>
2022-07-13 06:01:38 +0100

    some modifications

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Navbar from './Navbar';
 import Menu from './Menu';
 import AboutUs from './AboutUs';
+import Book from './Book';
+import Avis from './Avis';
 
 const App =  () => (
     <div className="App">
       <Navbar />
       <Menu />
       <AboutUs />
+      <Book />
+      <Avis />
       
     </div>
 );
