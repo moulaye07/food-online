b7c886c7b799228760b9a8513638433d78601745
moulaye07 <haidaramoulayetouhami@gmail.com>
2022-07-13 05:29:24 +0100

    add About us section

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './Navbar';
 import Menu from './Menu';
+import AboutUs from './AboutUs';
 
 const App =  () => (
     <div className="App">
       <Navbar />
       <Menu />
+      <AboutUs />
+      
     </div>
 );
 
